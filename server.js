cff9908ab20d61e2c4beb892de9dba54d140bdc2
DESKTOP-GR4Q87H\justi <justintan_14@hotmail.com>
2018-10-26 10:48:31 -0700

    User (backend): created User schema, routes, register/login, simple validation, JWT to create token and passport to authenticate token after login

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const users = require('./routes/api/users');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
@@ -15,6 +22,15 @@ mongoose
   )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+// Passpoer middlware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
+app.use('/api/users', users);
 
 // process.env.PORT is for deployment on Heroku
 const port = process.env.PORT || 5000;
